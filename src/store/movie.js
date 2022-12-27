6d735adad27a2138b194601b66f6d703d4f7951f
minhee <jmh3699@naver.com>
2022-12-27 21:02:20 +0900

    [4] 예외 처리와 메시지 출력
    영화 검색 시 검색되지 않는 키워드일 때 viewMore 버튼 없애고 로딩 애니메이션 보이지 않게 수정

--- src/store/movie.js
+++ src/store/movie.js
@@ -5,7 +5,8 @@ const store = new Store({
   page: 1,
   pageMax: 1,
   movies: [],
-  loading: false
+  loading: false,
+  message: 'Search for the movie title!'
 })
 
 export default store
@@ -14,14 +15,23 @@ export const searchMovies = async page => {
   store.state.page = page
   if (page === 1) { // 새 내용 검색 시 페이지 초기화 
     store.state.movies = []
+    store.state.message = ''
   }
-  const res = await fetch(`https://www.omdbapi.com/?apikey=d2ef87f2&s=${store.state.searchText}&page=${page}`)
-  const { Search, totalResults } = await res.json()
-  //console.log(Search)
-  store.state.movies = [
-    ...store.state.movies, // 누적
-    ...Search
-  ]
-  store.state.pageMax = Math.ceil(Number(totalResults) / 10)
-  store.state.loading = false
+  try {
+    const res = await fetch(`https://www.omdbapi.com/?apikey=d2ef87f2&s=${store.state.searchText}&page=${page}`)
+    const { Search, totalResults, Response, Error } = await res.json()
+    if (Response === 'True') {
+      store.state.movies = [
+        ...store.state.movies, // 누적
+        ...Search
+      ]
+      store.state.pageMax = Math.ceil(Number(totalResults) / 10)
+    } else {
+      store.state.message = Error
+    }
+  } catch (error) {
+    console.log('searchMovies error: ', error)
+  } finally {
+    store.state.loading = false
+  }
 }
