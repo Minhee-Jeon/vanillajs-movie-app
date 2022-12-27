8f690fad5180b8a0709474db4288e5805f9d4a13
minhee <jmh3699@naver.com>
2022-12-27 20:32:59 +0900

    [4] 영화 목록 더 가져오기
    view more 버튼 클릭시 불러올 수 있는 영화가 다 보여질 때까지 누적 렌더링될 수 있도록 작업

--- src/store/movie.js
+++ src/store/movie.js
@@ -3,20 +3,22 @@ import { Store } from '../core/heropy'
 const store = new Store({
   searchText: '',
   page: 1,
+  pageMax: 1,
   movies: []
 })
 
 export default store
 export const searchMovies = async page => {
-  if (page === 1) { // 새 내용 검색 시 페이지 초기화
-    store.state.page = 1
+  store.state.page = page
+  if (page === 1) { // 새 내용 검색 시 페이지 초기화 
     store.state.movies = []
   }
   const res = await fetch(`https://www.omdbapi.com/?apikey=d2ef87f2&s=${store.state.searchText}&page=${page}`)
-  const { Search } = await res.json()
+  const { Search, totalResults } = await res.json()
   //console.log(Search)
   store.state.movies = [
     ...store.state.movies, // 누적
     ...Search
   ]
+  store.state.pageMax = Math.ceil(Number(totalResults) / 10)
 }
